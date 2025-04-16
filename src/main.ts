fcb95e293df36d366012b9e9da06b4ccc3b75ec3
Arktosum <siddhujaykay2@gmail.com>
2025-04-16 18:05:58 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 
 import './style.css'
-const root = document.querySelector('#root');
+const root = document.querySelector('#root')!;
 
 const container = document.createElement('div')
 
@@ -8,12 +8,12 @@ container.id = 'container';
 
 
 enum Color {
-    WHITE,
-    RED,
-    BLUE,
     GREEN,
-    ORANGE,
+    RED,
     YELLOW,
+    ORANGE,
+    WHITE,
+    BLUE,
 }
 
 
@@ -32,8 +32,9 @@ let CUBE = [
     [Color.ORANGE, Color.ORANGE, Color.ORANGE, Color.ORANGE, Color.ORANGE, Color.ORANGE, Color.ORANGE, Color.ORANGE, Color.ORANGE],
     [Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE],
     [Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE],
-
 ];
+
+
 
 let row1 = document.createElement('div');
 let row2 = document.createElement('div');
@@ -42,11 +43,68 @@ let row3 = document.createElement('div');
 row1.id = 'row-1'
 row2.id = 'row-2'
 row3.id = 'row-3'
+
 container.appendChild(row1)
 container.appendChild(row2)
 container.appendChild(row3)
 root?.appendChild(container)
 
+
+
+function one_to_two(i) {
+    return [Math.floor(i / 3), i % 3]
+}
+
+function two_to_one(i, j) {
+    return 3 * i + j;
+}
+
+
+
+function swap(face, i, j) {
+    let temp = face[j];
+    face[j] = face[i];
+    face[i] = temp;
+}
+function swap3(face1, face2) {
+    let temp = [face2[0], face2[1], face2[2]];
+    face2[0], face2[1], face2[2] = face1[0], face1[1], face1[2]
+    face1[0], face1[1], face1[2] = temp;
+}
+
+const OPP_FACE_MAP = {
+    [Color.WHITE]: [Color.ORANGE, Color.BLUE, Color.RED, Color.GREEN],
+    [Color.RED]: [Color.WHITE, Color.BLUE, Color.YELLOW, Color.GREEN],
+    [Color.BLUE]: [Color.WHITE, Color.ORANGE, Color.YELLOW, Color.RED],
+    [Color.ORANGE]: [Color.WHITE, Color.GREEN, Color.YELLOW, Color.BLUE],
+    [Color.GREEN]: [Color.WHITE, Color.RED, Color.YELLOW, Color.ORANGE],
+    [Color.YELLOW]: [Color.RED, Color.BLUE, Color.ORANGE, Color.GREEN]
+}
+function rotateFace(face_index, clockwise = true) {
+    let face = CUBE[face_index];
+    // Rotate face!
+    let order = [1, 2, 5, 8, 7, 6, 3];
+    if (!clockwise) order.reverse()
+    for (let i = 0; i < 2; i++) {
+        for (let ord of order) {
+            swap(face, 0, ord);
+        }
+    }
+    let faces: number[] = OPP_FACE_MAP[face_index];
+
+    let orange = CUBE[faces[0]]
+    let blue = CUBE[faces[1]]
+    let orange_three = [orange[0],orange[1],orange[2]];
+    let blue_three = [blue[0],blue[1],blue[2]];
+    console.log(orange_three.map((item)=>color_to_text[item]))
+    console.log(blue_three.map((item)=>color_to_text[item]))
+
+    blue[0],blue[1],blue[2] = orange[0],orange[1],orange[2]
+    // Rotate sides 
+}
+
+
+rotateFace(Color.WHITE, true)
 
 let face = document.createElement('div');
 face.id = 'face'
@@ -81,3 +139,5 @@ for (let j = 0; j < 9; j++) {
     face.appendChild(cell)
 }
 row3.appendChild(face)
+
+
