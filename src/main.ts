b0195c8a70dc044d07d19a6a633adac09b58e842
Siddhu Jaykay <siddhujaykay2@gmail.com>
2025-05-02 07:33:48 +0530

    sync

--- src/main.ts
+++ src/main.ts
@@ -1,283 +1,137 @@
-
-import { Brain, Softmax } from './genetic';
-import Matrix from './matrix';
 import './style.css'
-const root = document.querySelector('#root')!;
-
-const container = document.createElement('div')
-
-container.id = 'container';
-
+const root = document.getElementById('root')!;
 
 enum Color {
-    GREEN,
-    RED,
-    YELLOW,
     ORANGE,
+    GREEN,
     WHITE,
     BLUE,
+    YELLOW,
+    RED
 }
-
-
-const color_to_text: Record<Color, string> = {
-    [Color.WHITE]: 'white',
-    [Color.RED]: 'red',
-    [Color.BLUE]: 'blue',
-    [Color.GREEN]: 'green',
-    [Color.ORANGE]: 'orange',
-    [Color.YELLOW]: 'yellow',
-}
-
-
-
-
-
-
-
-
-
-function swap(face: number[], i: number, j: number) {
-    let temp = face[j];
-    face[j] = face[i];
-    face[i] = temp;
-}
-
-
-enum Orientation {
+enum Direction {
+    CLOCKWISE,
+    ANTICLOCKWISE,
     TOP,
     BOTTOM,
     LEFT,
     RIGHT
 }
 
-let ORIENTATION_MAP = {
-    [Orientation.TOP]: [2, 1, 0],
-    [Orientation.BOTTOM]: [6, 7, 8],
-    [Orientation.LEFT]: [0, 3, 6],
-    [Orientation.RIGHT]: [8, 5, 2],
-}
-function swap3(cube: Color[][], from: [Color, Orientation], to: [Color, Orientation]) {
-    let [from_color, from_orientation] = from;
-    let [to_color, to_orientation] = to;
-    let from_face = cube[from_color];
-    let to_face = cube[to_color];
-
-    let from_indices = ORIENTATION_MAP[from_orientation];
-    let to_indices = ORIENTATION_MAP[to_orientation];
-    let temp = [to_face[to_indices[0]], to_face[to_indices[1]], to_face[to_indices[2]]];
-    for (let i = 0; i < 3; i++) {
-        to_face[to_indices[i]] = from_face[from_indices[i]];
-        from_face[from_indices[i]] = temp[i];
+function createCube() {
+    const cube: Record<Color, Color[]> = {
+        [Color.ORANGE]: new Array<Color>(9).fill(Color.ORANGE),
+        [Color.GREEN]: new Array<Color>(9).fill(Color.GREEN),
+        [Color.WHITE]: new Array<Color>(9).fill(Color.WHITE),
+        [Color.BLUE]: new Array<Color>(9).fill(Color.BLUE),
+        [Color.YELLOW]: new Array<Color>(9).fill(Color.YELLOW),
+        [Color.RED]: new Array<Color>(9).fill(Color.RED),
     }
+    return cube;
 }
 
 
+const COLOR_TO_STRING = {
+    [Color.ORANGE]: 'orange',
+    [Color.GREEN]: 'green',
+    [Color.WHITE]: 'white',
+    [Color.BLUE]: 'blue',
+    [Color.YELLOW]: 'yellow',
+    [Color.RED]: 'red',
+}
 
-const ADJ_FACE_MAP: Record<Color, [Color, Orientation][]> = {
-    [Color.WHITE]: [[Color.ORANGE, Orientation.TOP], [Color.BLUE, Orientation.TOP], [Color.RED, Orientation.TOP], [Color.GREEN, Orientation.TOP]],
-    [Color.RED]: [[Color.WHITE, Orientation.BOTTOM], [Color.BLUE, Orientation.LEFT], [Color.YELLOW, Orientation.TOP], [Color.GREEN, Orientation.RIGHT]],
-    [Color.BLUE]: [[Color.WHITE, Orientation.RIGHT], [Color.ORANGE, Orientation.LEFT], [Color.YELLOW, Orientation.RIGHT], [Color.RED, Orientation.RIGHT]],
-    [Color.ORANGE]: [[Color.WHITE, Orientation.TOP], [Color.GREEN, Orientation.LEFT], [Color.YELLOW, Orientation.BOTTOM], [Color.BLUE, Orientation.RIGHT]],
-    [Color.GREEN]: [[Color.WHITE, Orientation.LEFT], [Color.RED, Orientation.LEFT], [Color.YELLOW, Orientation.LEFT], [Color.ORANGE, Orientation.RIGHT]],
-    [Color.YELLOW]: [[Color.RED, Orientation.BOTTOM], [Color.BLUE, Orientation.BOTTOM], [Color.ORANGE, Orientation.BOTTOM], [Color.GREEN, Orientation.BOTTOM]]
+// TOP, RIGHT, BOTTOM, LEFT
+const ADJACENT_MAP: Record<Color, [Color, Direction][]> = {
+    [Color.ORANGE]: [[Color.WHITE, Direction.TOP], [Color.GREEN, Direction.LEFT], [Color.YELLOW, Direction.BOTTOM], [Color.BLUE, Direction.RIGHT]],
+    [Color.GREEN]: [[Color.WHITE, Direction.LEFT], [Color.RED, Direction.LEFT], [Color.YELLOW, Direction.LEFT], [Color.ORANGE, Direction.RIGHT]],
+    [Color.WHITE]: [[Color.ORANGE, Direction.TOP], [Color.BLUE, Direction.TOP], [Color.RED, Direction.TOP], [Color.GREEN, Direction.TOP]],
+    [Color.BLUE]: [[Color.WHITE, Direction.RIGHT], [Color.ORANGE, Direction.LEFT], [Color.YELLOW, Direction.TOP], [Color.RED, Direction.RIGHT]],
+    [Color.YELLOW]: [[Color.RED, Direction.BOTTOM], [Color.BLUE, Direction.BOTTOM], [Color.ORANGE, Direction.BOTTOM], [Color.GREEN, Direction.BOTTOM]],
+    [Color.RED]: [[Color.WHITE, Direction.BOTTOM], [Color.BLUE, Direction.LEFT], [Color.YELLOW, Direction.TOP], [Color.GREEN, Direction.RIGHT]],
+}
+function createFace(cube: Record<Color, Color[]>, color: Color) {
+    const face = document.createElement('div');
+    face.classList.add('face');
+    for (let i = 0; i < 9; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        let cellColor = cube[color][i];
+        cell.classList.add(COLOR_TO_STRING[cellColor]);
+        face.appendChild(cell);
+    }
+    return face;
+}
+
+function displayCube(cube: Record<Color, Color[]>) {
+
+    const cubeContainer = document.createElement('div');
+
+    const row1 = document.createElement('div');
+    const row2 = document.createElement('div');
+    const row3 = document.createElement('div');
+
+    row1.classList.add('flex');
+    row2.classList.add('flex');
+    row3.classList.add('flex');
+
+    const redFace = createFace(cube, Color.RED);
+    const blueFace = createFace(cube, Color.BLUE);
+    const greenFace = createFace(cube, Color.GREEN);
+    const whiteFace = createFace(cube, Color.WHITE);
+    const yellowFace = createFace(cube, Color.YELLOW);
+    const orangeFace = createFace(cube, Color.ORANGE);
+
+    row1.appendChild(orangeFace);
+
+    orangeFace.style.marginLeft = '305px'
+    redFace.style.marginLeft = '305px'
+    row2.appendChild(greenFace);
+    row2.appendChild(whiteFace);
+    row2.appendChild(blueFace);
+    row2.appendChild(yellowFace);
+
+    row3.appendChild(redFace);
+
+    cubeContainer.appendChild(row1);
+    cubeContainer.appendChild(row2);
+    cubeContainer.appendChild(row3);
+    cubeContainer.style.margin = '50px';
+    root.appendChild(cubeContainer);
 }
 
 
-
-
-function display_cube(cube: Color[][]) {
-    let row1 = document.createElement('div');
-    let row2 = document.createElement('div');
-    let row3 = document.createElement('div');
-
-    row1.id = 'row-1'
-    row2.id = 'row-2'
-    row3.id = 'row-3'
-
-    container.appendChild(row1)
-    container.appendChild(row2)
-    container.appendChild(row3)
-    root.appendChild(container)
-
-
-    let face = document.createElement('div');
-    face.id = 'face'
-    for (let j = 0; j < 9; j++) {
-        let cell = document.createElement('div');
-        cell.classList.add('cell')
-        if (j == 4) {
-            cell.innerText = '^'
-        }
-        cell.classList.add(color_to_text[cube[0][j]]);
-        face.appendChild(cell)
-    }
-    row1.appendChild(face)
-
-    for (let i = 1; i <= 4; i++) {
-        let face = document.createElement('div');
-        face.id = 'face'
-        for (let j = 0; j < 9; j++) {
-            let cell = document.createElement('div');
-            cell.classList.add('cell')
-            if (j == 4) {
-                cell.innerText = '^'
-            }
-            cell.classList.add(color_to_text[cube[i][j]]);
-            face.appendChild(cell)
-        }
-        row2.appendChild(face)
-    }
-
-
-    face = document.createElement('div');
-    face.id = 'face'
-    for (let j = 0; j < 9; j++) {
-        let cell = document.createElement('div');
-        cell.classList.add('cell')
-        if (j == 4) {
-            cell.innerText = '^'
-        }
-        cell.classList.add(color_to_text[cube[5][j]]);
-        face.appendChild(cell)
-    }
-    row3.appendChild(face)
+function swap(array: any[], i: number, j: number) {
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
 }
 
-
-
-
-
-
-function random_choice<T>(array: T[]) {
-    return array[Math.floor(Math.random() * array.length)];
-}
-const colors = [Color.WHITE, Color.BLUE, Color.GREEN, Color.YELLOW, Color.ORANGE, Color.YELLOW]
-const directions = [true, false];
-
-
-
-
-
-
-
-
-
-class Cube {
-    cube: Color[][];
-    constructor() {
-        this.cube = [
-            [Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN],
-            [Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED],
-            [Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW],
-            [Color.ORANGE, Color.ORANGE, Color.ORANGE, Color.ORANGE, Color.ORANGE, Color.ORANGE, Color.ORANGE, Color.ORANGE, Color.ORANGE],
-            [Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE],
-            [Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE],
-        ]
-    }
-    compareSolved() {
-        const solved_cube = [
-            [Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN],
-            [Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED],
-            [Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW],
-            [Color.ORANGE, Color.ORANGE, Color.ORANGE, Color.ORANGE, Color.ORANGE, Color.ORANGE, Color.ORANGE, Color.ORANGE, Color.ORANGE],
-            [Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE],
-            [Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE],
-        ]
-        let grid_counts = 0;
-        for (let i = 0; i < 6; i++) {
-            let grid_count = 0;
-            for (let j = 0; j < 9; j++) {
-                grid_count += (solved_cube[i][j] == this.cube[i][j]) ? 1 : 0
-            }
-            grid_counts += (grid_count / 9);
+function makeMove(cube: Record<Color, Color[]>, faceColor: Color, direction: Direction) {
+    let face = cube[faceColor];
+    let swapOrder = [1, 2, 5, 8, 7, 6, 3];
+    if (direction == Direction.ANTICLOCKWISE) swapOrder.reverse();
+    for (let i = 0; i < 2; i++) {
+        for (let index of swapOrder) {
+            swap(face, 0, index);
         }
-        return grid_counts / 6;
-
-    }
-    copy() {
-        return this.cube.map(row => [...row]);
-    }
-    scramble(move_count: number) {
-        let moves = [];
-        for (let i = 0; i < move_count; i++) {
-            const random_face = random_choice(colors);
-            const random_direction = random_choice(directions);
-            moves.push([color_to_text[random_face], random_direction ? 'clockwise' : 'counter-clockwise'])
-            this.rotateFace(random_face, random_direction);
-        }
-        return moves;
-    }
-    flatten() {
-        let flattened_cube: Color[] = [];
-        for (let face of this.cube) {
-            for (let cell of face) {
-                flattened_cube.push(cell);
-            }
-        }
-        return flattened_cube;
-    }
-    makeMove(index: number) {
-        if (index == 12) {
-            // Do nothing!
-            return
-        }
-        const face_color = colors[Math.floor(index / 2)];
-        const direction = directions[index % 2];
-        // console.log(color_to_text[face_color], direction ? 'cw' : 'ccw');
-        this.rotateFace(face_color, direction);
-    }
-    rotateFace(face_color: Color, clockwise: boolean) {
-        let face = this.cube[face_color];
-        // Rotate face!
-        let order = [1, 2, 5, 8, 7, 6, 3];
-        if (!clockwise) order.reverse()
-        for (let i = 0; i < 2; i++) {
-            for (let ord of order) {
-                swap(face, 0, ord);
-            }
-        }
-        // Rotate sides 
-
-        let adj_faces = ADJ_FACE_MAP[face_color];
-
-        order = [1, 2, 3];
-        if (!clockwise) order.reverse()
-        swap3(this.cube, adj_faces[0], adj_faces[order[0]]);
-        swap3(this.cube, adj_faces[0], adj_faces[order[1]]);
-        swap3(this.cube, adj_faces[0], adj_faces[order[2]]);
     }
 }
 
-let common_cube = new Cube();
-common_cube.scramble(20);
+let cube = createCube();
+cube[Color.WHITE][0] = Color.RED;
+cube[Color.WHITE][1] = Color.BLUE;
+cube[Color.WHITE][2] = Color.GREEN;
 
-let population_scores = []
-for (let child of POPULATION) {
-    let child_cube = new Cube();
-    child_cube.cube = common_cube.copy();
+cube[Color.WHITE][3] = Color.ORANGE;
+cube[Color.WHITE][4] = Color.WHITE;
+cube[Color.WHITE][5] = Color.YELLOW;
 
-    for (let i = 0; i < 100; i++) {
-        // Try making 100 moves!
-        const scrambled_cube_vector = Matrix.fromArray(child_cube.flatten());
-        const probs = child.forward(scrambled_cube_vector);
-        const move_index = Softmax.argmax(probs);
-        child_cube.makeMove(move_index);
-    }
-    let fitness_score = child_cube.compareSolved();
-    population_scores.push(fitness_score);
-}
+cube[Color.WHITE][6] = Color.GREEN;
+cube[Color.WHITE][7] = Color.RED;
+cube[Color.WHITE][8] = Color.BLUE;
 
-class Agent {
+displayCube(cube);
 
-}
+makeMove(cube, Color.WHITE, Direction.CLOCKWISE);
 
-class GeneticAlgorithm {
-    population_size: number;
-    constructor(population_size: number, agent: Agent) {
-        const POPULATION = [];
-        this.population_size = population_size;
-        for (let i = 0; i < this.population_size; i++) {
-            POPULATION.push(new Agent());
-        }
-    }
-}
+displayCube(cube);
